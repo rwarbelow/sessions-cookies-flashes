0ebabfe3159feb3be16fc2835a6898a22277a6df
Rachel Warbelow <rachelwarbelow@gmail.com>
2015-04-19 17:08:54 -0600

    fix flash examples

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
-  resources :users
-
   resources :cookie_monsters, only: [:index, :new, :create]
   delete '/cookie_monsters', to: 'cookie_monsters#destroy'
 
   resources :sessions, only: [:index, :new, :create]
+
+  resources :users, only: [:index, :new, :create]
 end
